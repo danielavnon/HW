ba3a9df57c6dd181a843dce22109c72e6f1b1888
danielavnon <82079073+danielavnon@users.noreply.github.com>
2021-04-11 21:16:55 +0300

    Update scrape_players.sh
    80 lines

--- HW1/scrape_players.sh
+++ HW1/scrape_players.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 #script argumets:
 #total_players: total players count
-#search_result: how many the specified 'Midfielder' has appeared within players' page
+#search_result: how many times 'Midfielder' has appeared within players' page
 #script name: scrape_players
 #these comments are divided linewise, keeping given commands commentless
 #(lines: 2,3) create a utility text file, which will hold all players' links
-#(line: 4) count how many links there are and save this count within 'total_players'
+#(line: 4) how many links there are and save this count as'total_players'
 #(line: 5) initiate a output file, its first line stating total players count
 #(line: 6) loop declaration, iterating for each link wihtin utility text file
 #(line: 8) wget creates 'overview' file, see complementary comment (line 13)
-#(line: 9) searching 'overview' file how many times 'Midfielder' field had appeared
+#(line: 9) searching in 'overview' how many times 'Midfielder' appeared
 #(line: 10) omitting players which did not match 'Midfielder' requirement
 #(line: 11) concatenates given string and search result for each matching link
-#(line: 13) deleting 'overview' file to ensure next iteration of line 11 execution will result in an iterated 'overview' file
+#(line: 13) deleting 'overview' file to ensure next iteration of line 11 execution
+#(line: 13) continue: will result in an iterated 'overview' file
 #(line: 15) printing the desired output file
 
 wget https://www.premierleague.com/players
-cat players | grep -oP "(/players/)[0-9]+/[a-zA-Z-]+(/overview)" players > urls.txt
+grep -oP "(/players/)[0-9]+/[a-zA-Z-]+(/overview)" players > urls.txt
 sed -i 's/\/players/https:\/\/www\.premierleague\.com\/players/' urls.txt
 total_players=`cat urls.txt | wc -l`
 echo "Total_players: $total_players" > Results.csv
