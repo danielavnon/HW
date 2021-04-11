8d010aa221f1e4d9128fa6b69a4f48229e47f307
danielavnon <82079073+danielavnon@users.noreply.github.com>
2021-04-11 20:24:56 +0300

    Results
    Results

--- scrape_players.sh
+++ scrape_players.sh
@@ -19,16 +19,16 @@ wget https://www.premierleague.com/players
 cat players | grep -oP "(/players/)[0-9]+/[a-zA-Z-]+(/overview)" players > urls.txt
 sed -i 's/\/players/https:\/\/www\.premierleague\.com\/players/' urls.txt
 total_players=`cat urls.txt | wc -l`
-echo "Total_players: $total_players" > results.csv
+echo "Total_players: $total_players" > Results.csv
 for link in `cat urls.txt`
 do
 	wget $link
 	search_result=`cat overview | grep -oP "Midfielder|midfielder+" | wc -l`
 	if [[ $search_result -gt 0 ]] ; then
-		echo "$link, Midfielder, $search_result" >> results.csv
+		echo "$link, Midfielder, $search_result" >> Results.csv
 	fi
 	rm overview
 done
-cat results.csv
+cat Results.csv
 rm players
 rm urls.txt
