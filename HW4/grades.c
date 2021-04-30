f7c43c31c58b4f822c537030502968d3754aa8e0
danielavnon <82079073+danielavnon@users.noreply.github.com>
2021-04-30 19:06:01 +0300

    update 2

--- HW4/grades.c
+++ HW4/grades.c
@@ -1,200 +1,227 @@
 
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "grades.h"
 #include "linked-list.h"
 
 #define MAX_GRADE 100
 #define MIN_GRADE 0
-
+#define ERROR 1
 
 struct grades {
-	struct grades* next;
-	struct grades* prev;
-	struct student_info* student; 
+	struct list* student_list;
 };
 
-struct student_info {
+struct student {
 	int id;
 	char* name;
-	struct grades_sheet* grades_sheet;
+	struct list* course_list;
 };
 
-
-struct grades_sheet {
-	struct grades_sheet* next;
-	struct grades_sheet* prev;
-	char *course_name;
-	int course_grade;
+struct course {
+	char *name;
+	int grade;
 };
 
-int clone_element (void *element, void **output){
+int clone_student (void *element, void **output){
 	
+	struct student *original= (struct student*)element;
+	struct student *replica;
+	replica->name=(char*)malloc(sizeof(char)*(strlen(original->name)+1));
 	
-	struct student_info *index= (struct student_info*)element;
-	struct student_info *clone_student;
-	//char *name=(char*)malloc(sizeof(char)*(strlen(index->name)+1));
-	
-	clone_student->name=(char*)malloc(sizeof(char)*(strlen(index->name)+1));
-	
-	if (!(clone_student->name)){
+	if (!(replica->name)){
 		//malloc error
-		return 1;
+		return ERROR;
 	}	
 	
-	strcpy(clone_student->name,index->name);
-	student_info->id=index->id;
-	stundent_info->grades_sheet=index->grades_sheet;
-	output=&clone_student;
+	strcpy(replica->name,original->name);
+	replica->id=original->id;
+	replica->course_list=original->course_list;
+	output=&replica;
 	
 	return 0;
 }
-/*
-	//initialisng a new student node and inserting student info
-	struct student_info new_student;
-	new_student.id=id;
-	new_student.name=malloc(sizeof(char)*(strlen(&name)+1));
+
+void destroy_student (void *element){
 	
-	if(!(new_student.name)){
-	//malloc error
-	return 1;
-	}
-	strcpy(new_student.name,name);
+	struct student *student_to_destroy=(struct student*)element;
+	free(student_to_destroy->name);
+	list_destroy(student_to_destroy->course_list);
+	free(student_to_destroy);
+}
+
+int clone_course (void *element, void **output){
 	
-	//a pointer which points at the newly made student node
-	struct student_info* p_new_student=&new_student;
-
-*/
-
-
-
-void destroy_element (void *element){
+	struct course *original= (struct course*)element;
+	struct course *replica;
+	replica->name=(char*)malloc(sizeof(char)*(strlen(original->name)+1));
 	
-	//free allocated memory
+	if (!(replica->name)){
+		//malloc error
+		return ERROR;
+	}	
+	
+	strcpy(replica->name,original->name);
+	replica->grade=original->grade;
+	output=&replica;
+	
+	return 0;
+}
+
+void destroy_course (void *element){
+	
+	struct course *course_to_destroy=(struct course*)element;
+	free(course_to_destroy->name);
+	free(course_to_destroy);
+}
+
+//initialising student list w/o course list
+struct grades* grades_init() {
+	
+	struct grades* grades;
+	
+	element_clone_t student_clone=clone_student;
+	element_destroy_t student_destroy=destroy_student;
+	
+	element_clone_t course_clone=clone_course;
+	element_destroy_t course_destroy=destroy_course;
+	
+	grades->student_list=list_init(student_clone,student_destroy);
+
+	return grades;
 	
 }
 
-
-struct grade* grades_init() {
-	
-	struct grades* grades;
-	element_clone_t elem_clone=clone_element;
-	element_destroy_t elem_destroy;
-	grades=list_init(elem_clone,elem_destroy);
-	
-	return grades;
-	
-};
-
 void grades_destroy(struct grades *grades){
 	
-	//using list_destroy and destroy_element
+	//using destroy_course and destroy_student
 }
 
 
 int grades_add_student(struct grades *grades, const char *name, int id) {
 		
 	if(!grades){
-		return 1;
+		return ERROR;
 	}
-	if (is_exists(grades,id,NULL)) { //NULL fails
-		return 1; //fail, as the student already exists
+	if (student_exists(grades->student_list,id,NULL)) { //NULL fails
+		return ERROR; //fail, as the student already exists
 	}
 	
-	//initialisng a new student node and inserting student info
-	struct student_info new_student;
+	//initialisng a new student node and feeding student info
+	struct student new_student;
 	new_student.id=id;
-	new_student.name=malloc(sizeof(char)*(strlen(&name)+1));
+	new_student.name=malloc(sizeof(char)*(strlen(name)+1));
 	
 	if(!(new_student.name)){
 	//malloc error
-	return 1;
+	return ERROR;
 	}
 	strcpy(new_student.name,name);
 	
 	//a pointer which points at the newly made student node
-	struct student_info* p_new_student=&new_student;
+	struct student* p_new_student=&new_student;
 	
-	if(!list_push_back(grades, p_new_student)){
-		return 1;
-	}
-	return 0;	
-};
+	return list_push_back(grades->student_list, p_new_student);
+}
 
 
 int grades_add_grades(struct grades *grades,const char *name,int id,int grade){
 	
 	if (!grades){
-		return 1;
-	}
-	struct grades* selected_student_node;
-	if (!is_exists(grades, id, selected_student_node)) {
-		return 1; //fail, as the student does not exists
+		return ERROR;
 	}
 	if (grade>MAX_GRADE|| grade<MIN_GRADE){
-		return 1;
+		return ERROR;
 	}
-	//checking if course exists in grades' sheet of selected student
-	struct grades_sheet* grades_sheet;
-	grades_sheet=(selected_student_node->student->grades_sheet);
+	struct student* selected_student_node;
+	if (!(student_exists(grades->student_list, id, selected_student_node))){
+		return ERROR;
+	}
+	//checking if course exists in grades' list of selected student
 	
-	struct grades_sheet *list_iterator=list_begin(grades_sheet);
-	struct grades_sheet *list_final_elem=list_end(grades_sheet);
-	while(list_iterator!=list_final_elem){
-		if(!strcmp(list_iterator->course_name,name)){
-			return 1;
-		}
-		list_iterator=list_next(grades_sheet);
+	//course=(selected_student_node->student->grades_sheet);
+	if(course_exists(selected_student_node->course_list,name)){
+		return ERROR;
 	}
-	if(!strcmp(list_final_elem->course_name,name)){
-		return 1;
-	}
+
+	struct course new_course;
 	
-	//initialising new grades_sheet node and pointing a pointer on it
-	struct grades_sheet new_course;
-	new_course.course_grade=grade;
-	new_course.name=malloc(sizeof(char)*(strlen(&name)+1));
+	new_course.grade=grade;
+	new_course.name=malloc(sizeof(char)*(strlen(name)+1));
 	if(!(new_course.name)){
 		//malloc error
-		return 1;
+		return ERROR;
 	}
 	strcpy(new_course.name,name);
-	struct grades_sheet* p_grades_sheet=&new_course;
 	
+	struct course* p_new_course=&new_course;
+	
+	return list_push_back(selected_student_node->course_list,p_new_course)
+}
 
-	if(!list_push_back(grades_sheet,p_grades_sheet)){
-		return 1;
-	}
-
-};
-
-
+/*
+float grades_calc_avg(struct grades *grades,int id,char **out){
 	
 	
 	
+	
+	
+	
+}	
+	
+int grades_print_student(struct grades *grades, int id){
+	
+	
+	
+	
+	
+}
 
+int grades_print_all(struct grades *grades){
+	
+	
+	
+	
+	
+}
+
+*/
 
 //1 means existing, 0 means non existing
 //if we attend to look whether a student is existing we use NULL pointer
 //if we attend to look to return a specific student pointer, we dont use NULL
 
-int is_exists(struct grades *grades,int id,struct grades *selected_student){
-	struct grades *list_iterator=list_begin(grades);
-	struct grades *list_final_elem=list_end(grades);
-	while (list_iterator!=list_final_elem) {
-		if (list_iterator->student->id==id) {
-			if (!selected_student){
-				selected_student=&list_iterator;
+int student_exists(struct list *list,int id,struct student *selected_student){
+	
+	struct iterator *student_iterator=list_begin(list);
+	struct student *current_student;
+	while(!student_iterator){
+		current_student=list_get(student_iterator);
+		if(current_student->id==id){
+			if(selected_student){
+				selected_student=current_student;
 			}
+			return 1;	
+		}
+		student_iterator=list_next(student_iterator);	
+	}
+	
+	return 0;	
+}
+	
+
+//1 means existing, 0 means non existing
+int course_exists(struct list* list, char* name){
+	struct iterator *course_iterator=list_begin(list);
+	struct course *current_course;
+	while(!course_iterator){
+		current_course=list_get(course_iterator);
+		if(!(strcmp(current_course->name,name))){
 			return 1;
 		}
-		list_iterator=list_next(grades);
-	}
-	if (list_final_elem->student->id==id) {
-		if (!selected_student){
-			selected_student=&list_iterator;
-		}
-		return 1;
+		course_iterator=list_next(course_iterator);
 	}
 	return 0;
 }
