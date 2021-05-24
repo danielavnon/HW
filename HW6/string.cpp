f4ab5532925caac7ae4c21b916c9ba90ffb07cec
danielavnon <82079073+danielavnon@users.noreply.github.com>
2021-05-24 10:06:36 +0300

    Update string.cpp

--- HW6/string.cpp
+++ HW6/string.cpp
@@ -1,49 +1,47 @@
+#include "string.h"
+#include <cstring>
+#include <iostream>
 
-char* String::allocate_and_copy(const char* str,int size){
-	return strcpy(new char[size+1],str);
-}
+char* allocate_and_copy(const char* str,int size);
 
 String::String(){
-	
-
+	data = NULL;
+	length = 0;
 }
 
-
-String:String(const String &str){
-	
-	length(str.len());
+String::String(const String &str){
+	length(str.length);
 	data(allocate_and_copy(str.data, str.length));
-	
 }
+
 String::String(const char *str){
-	
 	length = strlen(str);
-	data = allocate_and_copy(str, length);
-	
+	data = allocate_and_copy(str,length);
 }
 
-Srting::~String(){
-	delete[] data;
+String::~String(){
+	if(data != NULL){
+		delete[] data;
+	}
 }
 
-String::String& operator=(const String &rhs){
+String& String::operator=(const String &rhs){
 	if (this == &rhs){
 		return *this;
 	}
 	delete[] data;
-	data = allocate_and_copy(rhs.data,rhs.length);
 	length = rhs.length;
+	data = allocate_and_copy(rhs.data,length);
 	return *this;
-	
 }
 
-String::String& operator=(const char *str){
+String& String::operator=(const char *str){
 	if(this.data == str){
 		return *this;
 	}
 	delete[] data;
-	data = allocate_and_copy(str.data,str.length);
-	length = str.length;
+	length = stelen(str);
+	data = allocate_and_copy(str,length);
 	return *this;
 	
 	
@@ -78,7 +76,17 @@ int String::to_integer() const{
 	
 }
 
-String::String trim() const{
+String String::trim() const{
 	
 	
 }
+
+
+char* allocate_and_copy(const char* str,int size){
+
+	if(size == NULL){
+		return NULL;
+	}
+	else return strcpy(new char[size+1],str);
+}
+
