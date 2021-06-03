ab60b5de34e5587ff42d08beabc66d9fd0c7188d
dormishally <82078936+dormishally@users.noreply.github.com>
2021-06-03 20:31:35 +0300

    Update firewall.sh

--- HW6/firewall.sh
+++ HW6/firewall.sh
@@ -11,12 +11,11 @@ output=""
 		if [[ -n $rule ]]; then
 			readarray -d , -t filter <<< "$rule"
 			
-			feasible_rule=`echo "$input" | ./firewall.exe "$filter[0]" | ./firewall.exe "$filter[1]" | ./firewall.exe "$filter[2]" | ./firewall.exe "$filter[3]"`
-			
-			output+=`echo "$feasbile_rule" | sed '/^$/d' | sort -u`
+			feasible_rule=`echo "$input" | ./firewall.exe "${filter[0]}" | ./firewall.exe "${filter[1]}" | ./firewall.exe "${filter[2]}" | ./firewall.exe "${filter[3]}"`
+			output+=`echo "${feasbile_rule}" | sed '/^$/d' | sort -u `
 			output+="\n"
 		fi
 	done < "$1"
 	
-	echo -e "$output" | sed 's/ //g' | sed '/^$/d' | sort -u
+	echo -e "${output}" | sed 's/ //g' | sed '/^$/d' | sort -u
 		
