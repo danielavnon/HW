1ea355bf08b1fa1c3d58f350590b23b0d714c10c
danielavnon <82079073+danielavnon@users.noreply.github.com>
2021-06-05 22:17:04 +0300

    Update ip.h

--- HW6/ip.h
+++ HW6/ip.h
@@ -18,9 +18,31 @@ private:
 	// these two integers are referenced, disputed
 	
 public:
+	/**
+     * @brief Default constructor using a given variable
+     * @param pattern is a given String typed variable
+     */
 	Ip(String pattern);
+	
+	/**
+     * @brief sets the values to deter which Ip typed variable
+     * satisfy firewall settings
+     * @return true if set was complete, else false
+     */
 	bool set_value(String val);
+	
+		/**
+     * @brief checks if a given String typed variable is mathcing the
+     * firewall settings
+     * @return true if matching, else false
+     */
 	bool match_value(String val) const;
+	
+	/**
+     * @brief convers a String typed which containes the ip in its compact form
+     * @param ip_to_unite this is the variable which is to be converted
+     * @return int which is the converted form of the ip.
+     */
 	int united_ip(String ip_to_unite) const; // an explanation is the cpp file
 	
 };
